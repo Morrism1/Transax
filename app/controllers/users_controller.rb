bbf25edf1962ee35a8b21c13ea29e7767d1e97b8
Morrism1 <morrismontana0@gmail.com>
2020-11-19 17:23:11 +0200

    Create user resource generator and migration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Please confirm your email address to continue'
+      redirect_to @user
+    else
+      flash[:error] = 'Ooooppss, something went wrong!'
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
