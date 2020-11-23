da9df87134b52d582f5675618a3421f4508b6400
Morrism1 <morrismontana0@gmail.com>
2020-11-23 17:08:49 +0200

    Configure and add icon image uploader

---
+++ app/uploaders/group_icon_uploader.rb
@@ -0,0 +1,25 @@
+class GroupIconUploader < CarrierWave::Uploader::Base
+  if Rails.env == 'test'
+    storage :file
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  else
+    include Cloudinary::CarrierWave
+
+    process convert: 'png'
+    process tags: ['group_picture']
+
+    version :standard do
+      process resize_to_fill: [100, 150, :north]
+    end
+
+    version :thumbnail do
+      resize_to_fit(50, 50)
+    end
+
+    CarrierWave.configure do |config|
+      config.cache_storage = :file
+    end
+  end
+end
