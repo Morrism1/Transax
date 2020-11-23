6020676ed79c3a184a7d415729960ab8f08a1423
Morrism1 <morrismontana0@gmail.com>
2020-11-23 20:13:01 +0200

    Add associations to transaction model and validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
-  belongs_to :group
+  belongs_to :group, optional: true
+  belongs_to :user
+  has_many :groups
+
+  validates_presence_of :name, :amount
 end
