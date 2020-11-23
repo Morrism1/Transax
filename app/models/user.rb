1a94be098a0b43f63aefbb1ddc2494f97af3518d
Morrism1 <morrismontana0@gmail.com>
2020-11-23 16:10:37 +0200

    Add associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :transactions, foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, foreign_key: 'user_id', dependent: :destroy
+
+  before_save { self.name = name.downcase }
   validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
 end
