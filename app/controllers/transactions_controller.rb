8df18f9a0f12fe3091732f9b48a26f7b442d24a3
Morrism1 <morrismontana0@gmail.com>
2020-11-24 19:43:30 +0200

    Create transactions controller actions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,39 @@
 class TransactionsController < ApplicationController
+
+  def index
+    @transactions = current_user.transactions.includes(:group).where.not(group_id: nil).order(created_at: :desc)
+    @total = current_user.transactions.includes.where.not(group_id: nil).sum('amount')
+  end
+
+  def new
+    @transaction = current_user.transactions.build
+  end
+
+  def create
+    @transaction = current_user.transactions.build(transaction_params)
+    @transaction.author_id = current_user.id
+
+    if @transaction.save
+      flash[:notice] = "Transaction Successfully created..."
+      redirect transactions_path
+    else
+      flash[:alert] = "Can you try fill the fields again"
+      render :new
+    end
+  end
+
+  def external_transactions
+    @external = current_user.transactions.where(group_id: nil).order(created_at: :desc)
+    @total = current_user.transactions.where(group_id: nil).sum('amount')
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount)
+  end
 end
