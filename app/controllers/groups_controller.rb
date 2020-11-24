02890394179c2e5a9814daf8468a9f0a5ae62922
Morrism1 <morrismontana0@gmail.com>
2020-11-24 19:11:01 +0200

    Add tailwind classes to style user new page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,34 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all.order(created_at: :desc)
+  end
+
+  def new
+    @group = current_user.groups.build
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @transactions = @group.transactions.includes(:user)
+    @total = @group.transactions.sum('amount')
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      flash[:notice] = "Group Successfully created..."
+      redirect_to @group
+    else
+      render :new
+    end
+  end
+  
+  def destroy; end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
