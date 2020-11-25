365c6f11a09931714bcec99875380667505f7faa
Morrism1 <morrismontana0@gmail.com>
2020-11-25 15:18:36 +0200

    Add external transactions route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
+  get '/external',   to: 'transactions#external_transactions'
   resources :users
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
