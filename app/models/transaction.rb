a8d005a903e8f01bf98ca796fd56278f3671073e
Morrism1 <morrismontana0@gmail.com>
2020-11-26 16:32:53 +0200

    Fix association bug

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :group, optional: true
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :groups
 
   validates_presence_of :name, :amount
