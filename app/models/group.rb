80e84b1bf08af505e492cd77c6b95ec13fc8b61f
Morrism1 <morrismontana0@gmail.com>
2020-12-04 16:02:34 +0200

    Refactor associations in group model using a through table

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   mount_uploader :icon, GroupIconUploader
 
   belongs_to :user
-  has_many :transactions, dependent: :destroy
+  has_many :group_transactions, dependent: :destroy
+  has_many :transactions, through: :group_transactions, source: :deal
 
   validates_presence_of :name, :icon
 end
