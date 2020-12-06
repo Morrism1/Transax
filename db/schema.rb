f3947591e2cbdb2125c1b35c0aa1b80726c012e1
Morrism1 <morrismontana0@gmail.com>
2020-12-06 05:02:09 +0200

    Change null status on icon column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_005323) do
+ActiveRecord::Schema.define(version: 2020_12_06_024957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_005323) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
-    t.string "icon", null: false
+    t.string "icon"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
