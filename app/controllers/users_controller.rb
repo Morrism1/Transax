601e483cd001b5283683230356dfe9a1a77073f0
Morrism1 <morrismontana0@gmail.com>
2020-12-06 03:40:47 +0200

    Refactor flash messages in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Please confirm your email address to continue'
+      flash[:notice] = 'Logged in successful'
       redirect_to root_url
     else
-      flash[:error] = 'Ooooppss, something went wrong!'
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
       render 'new'
     end
   end
