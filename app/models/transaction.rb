a367873a6d84070fe58457bb73475bd515e6a2a7
Morrism1 <morrismontana0@gmail.com>
2020-12-06 05:26:30 +0200

    Add validations to amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,5 +3,6 @@ class Transaction < ApplicationRecord
   has_many :groups, through: :group_transactions
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  validates_presence_of :name, :amount
+  validates_presence_of :name
+  validates :amount, presence: true, numericality: { less_than: 1_000_000_000, greater_than: 0 }
 end
