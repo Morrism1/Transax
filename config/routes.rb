2749d99bd67469b3d8c8babd325260c268ad15a6
Morrism1 <morrismontana0@gmail.com>
2020-12-07 23:06:08 +0200

    Remove an used generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :groups
+  resources :transactions, only: %i[index create new]
+  resources :groups, only: %i[index new create show]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
-  get '/external',   to: 'transactions#external_transactions'
-  resources :users
+  get '/external', to: 'transactions#external_transactions'
+  resources :users, only: %i[new create show]
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
