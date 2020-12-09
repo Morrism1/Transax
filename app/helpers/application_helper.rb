64f9eb49ea999c88eba95366856d232c020a37ec
Morrism1 <morrismontana0@gmail.com>
2020-12-09 06:43:50 +0200

    Add flash messages inside helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+module ApplicationHelper 
+  def flash_class(level)
+    case level
+    when 'notice' then "bg-green-400"
+    when 'success' then "bg-green-500"
+    when 'error' then "bg-red-500"
+    when 'alert' then "bg-yellow-500"
+    end
+  end
 end
