d83cbac5a2f597630557e56103a1d9d5673b58f1
Morrism1 <morrismontana0@gmail.com>
2020-12-09 07:24:43 +0200

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
-module ApplicationHelper 
+module ApplicationHelper
   def flash_class(level)
-    case level
-    when 'notice' then "bg-green-400"
-    when 'success' then "bg-green-500"
-    when 'error' then "bg-red-500"
-    when 'alert' then "bg-yellow-500"
-    end
+    {
+      'notice' => 'bg-green-400',
+      'alert' => 'bg-yellow-400',
+      'error' => 'bg-red-500',
+      'success' => 'bg-green-500'
+    }[level]
   end
 end
